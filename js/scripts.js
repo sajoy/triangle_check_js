a89de6663f6a0714536daa7fc72078a536158171
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-13 09:25:31 -0800

    add equilateral and isosceles specs

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
 var triangleCheck = function(side1, side2, side3) {
+  var triangle = "";
 
-  return "not a triangle"
-
+  if (side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2) {
+    if (side1 === side2 && side2 === side3) {
+      triangle = "equilateral";
+    } else if (side1 === side2 || side1 === side3 || side3 === side2) {
+      triangle = "isosceles";
+    }
+  } else {
+    triangle = "not a triangle";
+  }
+  return triangle;
 };
--- spec/specs.js
+++ spec/specs.js
@@ -3,4 +3,12 @@ describe('triangleCheck', function() {
     expect(triangleCheck(2,2,8)).to.equal("not a triangle");
   });
 
+  it('will return "equilateral" if all sides are equal', function() {
+    expect(triangleCheck(3,3,3)).to.equal("equilateral");
+  });
+
+  it('will return "isosceles" if two sides are equal', function() {
+    expect(triangleCheck(6,6,4)).to.equal("isosceles");
+  });
+
 });
