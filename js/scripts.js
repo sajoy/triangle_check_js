b118120f7df0a48f558cd160c731c6b412a07c7f
Sarah Joy Calpo <sajoy.pc@gmail.com>
2015-02-13 10:10:06 -0800

    add html file and css

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var triangleCheck = function(side1, side2, side3) {
   var triangle = "";
 
-  if (side1 + side2 >= side3 && side2 + side3 >= side1 && side1 + side3 >= side2) {
+  if (side1 + side2 > side3 && side2 + side3 > side1 && side1 + side3 > side2) {
     if (side1 === side2 && side2 === side3) {
       triangle = "equilateral";
     } else if (side1 === side2 || side1 === side3 || side3 === side2) {
@@ -12,6 +12,6 @@ var triangleCheck = function(side1, side2, side3) {
   } else {
     triangle = "not a triangle";
   }
-  
+
   return triangle;
 };
